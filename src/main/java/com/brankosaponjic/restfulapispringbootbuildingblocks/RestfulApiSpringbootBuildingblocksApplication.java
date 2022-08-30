b5390f0ba9c29460589b4ee7b8d76bb6f27f2daf
Branko Saponjic <branko.saponjic@programerika.com>
2022-08-30 10:15:54 +0200

    Initial commit

---
+++ src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.brankosaponjic.restfulapispringbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSpringbootBuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brankosaponjic.restfulapispringbootbuildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSpringbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
