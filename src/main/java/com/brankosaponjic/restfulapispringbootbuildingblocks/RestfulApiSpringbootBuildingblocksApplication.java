b2287cf2bdc2be5ac485f986b73846cd751f9490
Branko Saponjic <branko.saponjic@programerika.com>
2022-08-30 14:51:33 +0200

    Added CommandLineRunner in RestfulApiSpringbootBuildingblocksApplication.java

--- src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplication.java
+++ src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplication.java
@@ -1,13 +1,31 @@
 package com.brankosaponjic.restfulapispringbootbuildingblocks;
 
+import com.brankosaponjic.restfulapispringbootbuildingblocks.entities.User;
+import com.brankosaponjic.restfulapispringbootbuildingblocks.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestfulApiSpringbootBuildingblocksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulApiSpringbootBuildingblocksApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(UserRepository repository) {
+        return (args) -> {
+            insertThreeUsers(repository);
+            System.out.println(repository.findAll());
+        };
+    }
+    private void insertThreeUsers(UserRepository repository) {
+        repository.save(new User(101L, "kreddy","Kalyan", "Reddy",  "kreddy@stacksimplify.com", "admin", "ssn101"));
+        repository.save(new User(102L, "gwiser", "Greg", "Wiser", "gwiser@stacksimplify.com", "admin", "ssn102"));
+        repository.save(new User(103L, "dmark", "David", "Mark", "dmark@stacksimplify.com", "admin", "ssn103"));
+
     }
 
 }
