b5c02615f3752d32e8c33b85ebf8bc8ee2bdd3be
Branko Saponjic <branko.saponjic@programerika.com>
2022-08-30 15:05:23 +0200

    Added services layer and UserService.java with CRUD methods

---
+++ src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/services/UserService.java
@@ -0,0 +1,40 @@
+package com.brankosaponjic.restfulapispringbootbuildingblocks.services;
+
+import com.brankosaponjic.restfulapispringbootbuildingblocks.entities.User;
+import com.brankosaponjic.restfulapispringbootbuildingblocks.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUserById(User user, Long id) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        if (userRepository.findById(id).isPresent()) {
+            userRepository.deleteById(id);
+        }
+    }
+}
