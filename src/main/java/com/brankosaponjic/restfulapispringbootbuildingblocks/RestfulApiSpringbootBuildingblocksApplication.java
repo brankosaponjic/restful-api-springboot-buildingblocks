04530e44e694cac744649af1052a518ad94c1fec
Branko Saponjic <branko.saponjic@programerika.com>
2022-08-30 10:18:54 +0200

    Added basic adjustments for H2 database

--- src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplication.java
+++ src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/RestfulApiSpringbootBuildingblocksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulApiSpringbootBuildingblocksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulApiSpringbootBuildingblocksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApiSpringbootBuildingblocksApplication.class, args);
+    }
 
 }
