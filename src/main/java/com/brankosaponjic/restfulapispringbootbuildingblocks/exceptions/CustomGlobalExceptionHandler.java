5771171bbea3096707d4f41bcb7b2e505f2bc9a7
Branko Saponjic <branko.saponjic@programerika.com>
2022-09-04 12:07:39 +0200

    Added as well as implemented HttpRequestMethodNotSupportedException

--- src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/exceptions/CustomGlobalExceptionHandler.java
+++ src/main/java/com/brankosaponjic/restfulapispringbootbuildingblocks/exceptions/CustomGlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.brankosaponjic.restfulapispringbootbuildingblocks.exceptions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -13,7 +14,7 @@ import java.util.Date;
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    //MethodArgumentNotValidException
+    // MethodArgumentNotValidException
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
@@ -29,5 +30,22 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         );
 
         return new ResponseEntity<>(customErrorDetails,HttpStatus.BAD_REQUEST);
+    }
+
+    // HttpRequestMethodNotSupportedException
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        CustomErrorDetails customErrorDetails = new CustomErrorDetails(
+                new Date(),
+                "Method Not Allowed. From MethodArgumentNotValidException in CustomGlobalExceptionHandler",
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
